7928bcaa1a391b5d5d0126ff62e7c955f5b45505
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2023-07-11 13:10:49 +0900

    新規追加

---
+++ src/main/java/jp/livlog/Client.java
@@ -0,0 +1,12 @@
+package jp.livlog;
+
+import org.teavm.jso.dom.html.HTMLDocument;
+
+public class Client {
+    public static void main(String[] args) {
+        var document = HTMLDocument.current();
+        var div = document.createElement("div");
+        div.appendChild(document.createTextNode("TeaVM generated element"));
+        document.getBody().appendChild(div);
+    }
+}
